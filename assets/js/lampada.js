365b1ab156a41a25aefe5e1d7785a2fd6e985b73
Lucas Davi <116373520+LDavi05@users.noreply.github.com>
2023-04-04 22:12:07 -0300

    Atualizar o lampada.js

--- assets/js/lampada.js
+++ assets/js/lampada.js
@@ -10,12 +10,16 @@ const isLampBroken = () => {
 const lampON = () => {
   if (!isLampBroken()) {
     lamp.src = './assets/img/ligada.png';
+    buttonOn.style.borderBottom = 'none';
+    buttonOff.style.borderBottom = '7px solid #00000034';
   }
 }
 
 const lampOFF = () => {
   if (!isLampBroken()) {
     lamp.src = './assets/img/desligada.png';
+    buttonOff.style.borderBottom = 'none';
+    buttonOn.style.borderBottom = '7px solid #00000034'
   }
 }
 
@@ -26,11 +30,14 @@ const lampBROKE = () => {
   buttonOff.classList.add('hid');
 }
 
+const reload = () => {
+  buttonReset.style.borderBottom = 'none';
+  location.reload();
+}
+
 buttonOn.addEventListener('click', lampON);
 buttonOff.addEventListener('click', lampOFF);
 lamp.addEventListener('mouseover', lampON);
 lamp.addEventListener('mouseleave', lampOFF);
 lamp.addEventListener('dblclick', lampBROKE);
-buttonReset.addEventListener('click', () => {
-  location.reload();
-});
+buttonReset.addEventListener('click', reload);
